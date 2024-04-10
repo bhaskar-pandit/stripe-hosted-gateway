9b97562a707aabaa190a4cd6d8c4959d9fc38494
Bhaskar <bhaskar.pandit@codeclouds.in>
2024-04-10 15:47:55 +0530

    git update

--- stripe-hosted-gateway.php
+++ stripe-hosted-gateway.php
@@ -130,34 +130,6 @@ function my_plugin_check_update_callback() {
     require "check-update/update.php";
 }
 
-add_action( 'init', 'github_plugin_updater_test_init' );
-function github_plugin_updater_test_init() {
-
-	include_once 'updater.php';
-
-	define( 'WP_GITHUB_FORCE_UPDATE', true );
-
-	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-
-		$config = array(
-			'slug' => plugin_basename( __FILE__ ),
-			'proper_folder_name' => 'stripe-hosted-gateway',
-			'api_url' => 'https://api.github.com/repos/bhaskar-pandit/stripe-hosted-gateway',
-			'raw_url' => 'https://raw.github.com/bhaskar-pandit/stripe-hosted-gateway/dibyendu',
-			'github_url' => 'https://github.com/bhaskar-pandit/stripe-hosted-gateway',
-			'zip_url' => 'https://github.com/bhaskar-pandit/stripe-hosted-gateway/archive/dibyendu.zip',
-			'sslverify' => true,
-			'requires' => '1.0',
-			'tested' => '1.0',
-			'readme' => 'README.md',
-			'access_token' => '',
-		);
-
-		new WP_GitHub_Updater( $config );
-
-	}
-
-}
 
 
 ?>
